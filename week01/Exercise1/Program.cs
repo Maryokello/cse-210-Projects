a2f38afcf55c310692265a7b7d951e235f2216b8
Mary Okello <okellomary2012@gmail.com>
2025-07-02 17:04:52 +0300

    added exercise3

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -82,6 +82,45 @@ class Program
         else
         {
             Console.WriteLine("You have the potential to do better!");
+            Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+            string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            // Generate a new random number each time
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+
+            int guessCount = 0;
+            int guessNumber = -1;
+
+            Console.Write("Guess the magic number between 1 and 100: ");
+
+            // Keep looping until the user guesses correctly
+            while (guessNumber != number)
+            {
+                guessNumber = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guessNumber > number)
+                {
+                    Console.WriteLine("Lower!");
+                }
+                else if (guessNumber < number)
+                {
+                    Console.WriteLine("Higher!");
+                }
+            }
+
+            Console.WriteLine($"You guessed it in {guessCount} attempts!");
+
+            // Ask to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
         }
+
+        Console.WriteLine("Thanks for playing!");
     }
-}
+   } 
+   }
