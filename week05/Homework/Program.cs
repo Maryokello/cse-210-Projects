348206be0532f7b16b7bf3e74f9c5a351290e134
Mary Okello <okellomary2012@gmail.com>
2025-08-03 14:46:28 +0300

    created the homework ptoject files

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,19 @@
+﻿// Program.cs
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Mary Okello", "C# Classes and Inheritance");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment math = new MathAssignment("Mary Okello", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment writing = new WritingAssignment("Mary Okello", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
