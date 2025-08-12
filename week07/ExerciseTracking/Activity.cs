96b4a7d05349030c08b5e0026f36318652f315de
Mary Okello <okellomary2012@gmail.com>
2025-08-12 20:41:47 +0300

    Added exercise tracking files

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,28 @@
+using System;
+
+abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public DateTime Date => _date;
+    public int Minutes => _minutes;
+
+    public abstract double GetDistance(); // in km
+    public abstract double GetSpeed();    // in kph
+    public abstract double GetPace();     // min per km
+
+    public virtual string GetSummary()
+    {
+        return $"{Date:dd MMM yyyy} {GetType().Name} ({Minutes} min): " +
+               $"Distance {GetDistance():0.0} km, " +
+               $"Speed {GetSpeed():0.0} kph, " +
+               $"Pace: {GetPace():0.0} min per km";
+    }
+}
---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Running : Activity
+{
+    private double _distance; // in km
+
+    public Running(DateTime date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance() => _distance;
+
+    public override double GetSpeed() => (GetDistance() / Minutes) * 60;
+
+    public override double GetPace() => Minutes / GetDistance();
+}
